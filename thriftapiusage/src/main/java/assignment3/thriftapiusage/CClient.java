707e805eac09ab7658d85c303cef7ee8ec08e34b
Pranjal Jain <jainpranj@gmail.com>
2017-06-11 22:12:43 -0400

    adding code for thrift api

--- thriftapiusage/src/main/java/assignment3/thriftapiusage/CClient.java
+++ thriftapiusage/src/main/java/assignment3/thriftapiusage/CClient.java
@@ -1,8 +1,13 @@
 package assignment3.thriftapiusage;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
 import java.util.List;
+import java.util.UUID;
 
 import org.apache.cassandra.thrift.Cassandra;
 import org.apache.cassandra.thrift.Column;
@@ -23,65 +28,99 @@ import org.apache.thrift.transport.TFramedTransport;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 
-public class CClient
-{
-    public static void main(String[] args)
-    throws TException, InvalidRequestException, UnavailableException, UnsupportedEncodingException, NotFoundException, TimedOutException
-    {
-        TTransport tr = new TFramedTransport(new TSocket("localhost", 9160));
-        TProtocol proto = new TBinaryProtocol(tr);
-        Cassandra.Client client = new Cassandra.Client(proto);
-        tr.open();
+import com.datastax.driver.core.BatchStatement;
+import com.datastax.driver.core.BoundStatement;
+import com.datastax.driver.core.PreparedStatement;
+import com.datastax.driver.core.Session;
 
-        String key_user_id = "1";
+public class CClient {
+	public static void main(String[] args) throws TException,
+			InvalidRequestException, UnavailableException,
+			NotFoundException, TimedOutException, FileNotFoundException, IOException {
+		TTransport tr = new TFramedTransport(new TSocket("localhost", 9160));
+		TProtocol proto = new TBinaryProtocol(tr);
+		Cassandra.Client client = new Cassandra.Client(proto);
+		tr.open();
 
-        // insert data
-        long timestamp = System.currentTimeMillis();
-        client.set_keyspace("Keyspace1");      
-        ColumnParent parent = new ColumnParent("Standard1");
+		client.set_keyspace("pranjal_keyspace");
+		ColumnParent parent = new ColumnParent("ratings");
+		readBookRatingCSVFile(parent, client);
+		tr.close();
+	}
 
-        Column nameColumn = new Column(toByteBuffer("name"));
-        nameColumn.setValue(toByteBuffer("Chris Goffinet"));
-        nameColumn.setTimestamp(timestamp);
-        client.insert(toByteBuffer(key_user_id), parent, nameColumn, ConsistencyLevel.ONE);
-        
-        Column ageColumn = new Column(toByteBuffer("age"));
-        ageColumn.setValue(toByteBuffer("24"));
-        ageColumn.setTimestamp(timestamp);
-        client.insert(toByteBuffer(key_user_id), parent, ageColumn, ConsistencyLevel.ONE);
+	public static ByteBuffer toByteBuffer(String value)
+			throws UnsupportedEncodingException {
+		return ByteBuffer.wrap(value.getBytes("UTF-8"));
+	}
 
-        ColumnPath path = new ColumnPath("Standard1");
+	public static String toString(ByteBuffer buffer)
+			throws UnsupportedEncodingException {
+		byte[] bytes = new byte[buffer.remaining()];
+		buffer.get(bytes);
+		return new String(bytes, "UTF-8");
+	}
 
-        // read single column
-        path.setColumn(toByteBuffer("name"));
-        System.out.println(client.get(toByteBuffer(key_user_id), path, ConsistencyLevel.ONE));
+	public static void readBookRatingCSVFile(ColumnParent parent,
+			Cassandra.Client client) throws FileNotFoundException, IOException,
+			InvalidRequestException, NotFoundException, UnavailableException,
+			TimedOutException, TException {
 
-        // read entire row
-        SlicePredicate predicate = new SlicePredicate();
-        SliceRange sliceRange = new SliceRange(toByteBuffer(""), toByteBuffer(""), false, 10);
-        predicate.setSlice_range(sliceRange);
-        
-        List<ColumnOrSuperColumn> results = client.get_slice(toByteBuffer(key_user_id), parent, predicate, ConsistencyLevel.ONE);
-        for (ColumnOrSuperColumn result : results)
-        {
-            Column column = result.column;
-            System.out.println(toString(column.name) + " -> " + toString(column.value));
-        }
+		// session.execute("CREATE TABLE pranjal_keyspace.ratings ("
+		// + "id uuid PRIMARY KEY," + "user_id text," + "isbn text,"
+		// + "book_rating text," + ");");
 
-        tr.close();
-    }
-    
-    public static ByteBuffer toByteBuffer(String value) 
-    throws UnsupportedEncodingException
-    {
-        return ByteBuffer.wrap(value.getBytes("UTF-8"));
-    }
-        
-    public static String toString(ByteBuffer buffer) 
-    throws UnsupportedEncodingException
-    {
-        byte[] bytes = new byte[buffer.remaining()];
-        buffer.get(bytes);
-        return new String(bytes, "UTF-8");
-    }
+		String csvFile = "/Users/pranjal/Desktop/cassandra-basics/BX-CSV-Dump/BX-Book-Ratings.csv";
+		String line = "";
+		String cvsSplitBy = ";";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+			while ((line = br.readLine()) != null) {
+
+				String[] tags = line.split(cvsSplitBy);
+				String key_user_id = String.valueOf(UUID.randomUUID());
+				// insert data
+				long timestamp = System.currentTimeMillis();
+
+				Column nameColumn = new Column(toByteBuffer("user_id"));
+				nameColumn.setValue(toByteBuffer(tags[0].replaceAll("\"", "")));
+				nameColumn.setTimestamp(timestamp);
+				client.insert(toByteBuffer(key_user_id), parent, nameColumn,
+						ConsistencyLevel.ONE);
+
+				Column ageColumn = new Column(toByteBuffer("isbn"));
+				ageColumn.setValue(toByteBuffer(tags[1].replaceAll("\"", "")));
+				ageColumn.setTimestamp(timestamp);
+				client.insert(toByteBuffer(key_user_id), parent, ageColumn,
+						ConsistencyLevel.ONE);
+				Column book_rating = new Column(toByteBuffer("book_rating"));
+				book_rating.setValue(toByteBuffer(tags[2].replaceAll("\"", "")));
+				book_rating.setTimestamp(timestamp);
+				client.insert(toByteBuffer(key_user_id), parent, book_rating,
+						ConsistencyLevel.ONE);
+
+				ColumnPath path = new ColumnPath("ratings");
+
+				// read single column
+				path.setColumn(toByteBuffer("user_id"));
+				System.out.println(client.get(toByteBuffer(key_user_id), path,
+						ConsistencyLevel.ONE));
+				// read entire row
+				SlicePredicate predicate = new SlicePredicate();
+				SliceRange sliceRange = new SliceRange(toByteBuffer(""),
+						toByteBuffer(""), false, 10);
+				predicate.setSlice_range(sliceRange);
+
+				List<ColumnOrSuperColumn> results = client.get_slice(
+						toByteBuffer(key_user_id), parent, predicate,
+						ConsistencyLevel.ONE);
+				for (ColumnOrSuperColumn result : results) {
+					Column column = result.column;
+					System.out.println(toString(column.name) + " -> "
+							+ toString(column.value));
+				}
+			}
+
+		}
+
+	}
 }
